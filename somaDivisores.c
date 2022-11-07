415efc0427ab9947361aba2b217f939489098cd6
Luís <lgsm4769@gmail.com>
2022-11-06 23:27:43 -0300

    trab 5 ok

---
+++ somaDivisores.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char** argv){
+    int num, divisor, soma;
+    printf("Digite um numero: \n");
+    scanf("%d", &num);
+
+    for( divisor = num ; divisor > 0; divisor--){
+        if( (num % divisor) == 0){
+            soma = soma + divisor;
+        }
+
+    };
+
+    printf("Soma dos divisores de %d = %d", num, soma);
+    return 0;
+
+}
