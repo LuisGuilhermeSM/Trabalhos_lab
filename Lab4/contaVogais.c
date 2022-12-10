965bbf14b9b963b410b0913e3a1d49517c2778d3
Luís <lgsm4769@gmail.com>
2022-12-10 14:44:44 -0300

    trab 1 ok

--- Lab4/contaVogais.c
+++ Lab4/contaVogais.c
@@ -1,29 +1,65 @@
 #include <stdio.h>
-#define N 5
+#include <stdlib.h>
 
-void get_str( char * str, int tam_str);
+char * inciar_string();
+int conta_vogais(char * str);
 
-int main(int argc ,char** argv){
-    char texto[N];
-    get_str(texto, N);
+int main(int argc, char** argv){
+    char * frase;
+    printf("Digite a frase desejada: \n");;
+    frase = inciar_string();
+    printf("%d",conta_vogais(frase));
+    
+    
+    free(frase);
 
     return 0;
+}
+
+char * inciar_string(){
+    char str;
+    char *string = (char *) malloc(1);
+    int tam = 0;
+    
+    while(1){
+        str = getchar();
+        if( str != '\n' ){
+            string = (char *) realloc(string, (tam+ 1) * sizeof(char) );
+            string[tam] = str;
+            tam += 1;
+        }
+        else{
+            string = (char *) realloc(string, (tam + 1) * sizeof(char) );
+            string[tam] = '\0';
+            tam += 1;
+            break;
+
+        }
+        
+    }
+
+
+
+    return string;
 
 }
 
-void get_str(char * str, int tam_str){
-    int i;
-    char c;
-    printf("Digite algo: \n");
-    for(i = 0; i < tam_str; i++){
-        c = getchar();
-        if(c != '\n'){
-            str[i] = c;
+
+int conta_vogais(char * str){
+    int cont = 0, tam = 0;
+    while( *(str + tam) != '\0'){
+        
+        if( ((*(str + tam) == 'a') || (*(str + tam) == 'e') || (*(str + tam) == 'i') || (*(str + tam) == 'o') || (*(str + tam) == 'u')) ){
+            cont += 1;
+        } 
+        else if( (*(str + tam) == 'A') || (*(str + tam) == 'E') || (*(str + tam) == 'I') || (*(str + tam) == 'O') || (*(str + tam) == 'U') ){
+            cont += 1;
         }
-        else{
-            str[i] = '\0';
-            break;
-        }
-        printf("%c", c);
+        
+        tam += 1;
+
     }
+    
+    return cont;
+
 }
